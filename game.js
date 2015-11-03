ca907a12882bd4b5b2e08dd3aa37936a24227933
Anton <tzat.anton@gmail.com>
2015-11-03 11:40:46 +0200

    Version 0.0.1

--- game.js
+++ game.js
@@ -5,9 +5,30 @@ var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAni
 element.width=500;
 element.height=500;
 
+var array = [];
+var color = [];
+
+for (var a =0; a< 10;a++){
+	color.push("rgb("+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+","+Math.floor(Math.random()*255)+")");
+}
+
+function block(x,y,cl){
+	this.x = x;
+	this.y = y;
+	this.cl = cl;
+}
+
+
+for(var i = 0, length1 = 50*50; i < length1; i++){
+	array.push({x:i%50,y:Math.floor(i/50),color:color[Math.floor(Math.random()*color.length)]});
+}
+
+
 function draw(){
-	context.fillStyle = "red";
-	context.fillRect(10,10,10,10);
+	for (var a =0; a< 50*50;a++){
+		context.fillStyle = array[a].color;
+		context.fillRect(array[a].x*10,array[a].y*10,10,10);
+	}
 	requestAnimationFrame(draw);
 }
 draw();
